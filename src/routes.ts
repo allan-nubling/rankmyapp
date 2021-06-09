be6c7bb4bc1509b3a01f10c04a4ecbdb1f4bd3c5
Allan Nubling <nubling@gmail.com>
2021-06-09 13:22:16 -0300

    teste concluido

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,15 @@ import { Router } from 'express'
 
 import ExampleController from '@controllers/ExampleController'
 
+import CharactersController from './controllers/CharactersController'
+
 const routes = Router()
 
 routes.get('/', async (_req, res) => {
     res.send('⚡️[server]: Server is running')
 })
+
+routes.get('/character', CharactersController.index)
 
 routes.post('/example', ExampleController.save)
 
